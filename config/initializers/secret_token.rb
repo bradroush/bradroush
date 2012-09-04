3df89faba7795c09c2aa293006e2083cad59b3b1
Brad Roush <roush.brad@gmail.com>
2012-09-04 14:39:21 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bradroush::Application.config.secret_token = 'ef804d3f8bc40f6924920c7680a052c0f6c3f9c61f36c54bdd9b81c2d93354f8227791da1c3efce9511645a6864d39d1067230f87e40df6c112e11f2a035ee6b'
